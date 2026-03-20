a5aee6ab80c65043fd007c89d7931f85e56b98af
Samu <sgomez@tactech.cl>
2026-03-19 22:06:07 -0300

    fix signin view and lougout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  def new
-    render inertia: "auth/login", props: {
-      submit_url: user_session_path,
-    }
-  end
+  # def new
+  #   render inertia: "auth/login", props: {
+  #     submit_url: user_session_path,
+  #   }
+  # end
 
-  def create
-    self.resource = warden.authenticate(auth_options)
-    if resource
-      set_flash_message!(:notice, :signed_in)
-      sign_in(resource_name, resource)
-      redirect_to after_sign_in_path_for(resource)
-    else
-      render inertia: "auth/login", props: {
-        submit_url: user_session_path,
-        errors: { base: [I18n.t("devise.failure.invalid")] },
-      }, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   self.resource = warden.authenticate(auth_options)
+  #   if resource
+  #     set_flash_message!(:notice, :signed_in)
+  #     sign_in(resource_name, resource)
+  #     redirect_to after_sign_in_path_for(resource)
+  #   else
+  #     render inertia: "auth/login", props: {
+  #       submit_url: user_session_path,
+  #       errors: { base: [I18n.t("devise.failure.invalid")] },
+  #     }, status: :unprocessable_entity
+  #   end
+  # end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
