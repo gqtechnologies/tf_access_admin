fe61797593f587c222d43052f24250cf71865e46
Samu <sgomez@tactech.cl>
2026-05-10 19:38:38 -0400

    fix db after commites

--- db/migrate/20260426200000_create_icons.rb
+++ db/migrate/20260426200000_create_icons.rb
@@ -2,12 +2,12 @@
 
 class CreateIcons < ActiveRecord::Migration[8.1]
   def change
-    create_table :icons, id: :uuid do |t|
+    create_table :icons, id: :uuid, if_not_exists: true do |t|
       t.string :name, null: false
 
       t.timestamps
     end
 
-    add_index :icons, :name, unique: true
+    add_index :icons, :name, unique: true, if_not_exists: true
   end
 end
