3a257cdbc4baf8e54fa5705e211021b21ba6b756
Samu <sgomez@tactech.cl>
2026-06-14 23:13:40 -0400

    fix unit tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,10 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
+    setup do
+      I18n.locale = I18n.default_locale
+    end
+
     # Add more helper methods to be used by all tests here...
   end
 end
